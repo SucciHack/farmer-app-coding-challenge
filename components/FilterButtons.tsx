da4d4f652ea03f0043f6ee6eb51c2e3a1b979682
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-01-17 00:02:12 +0300

    updated

--- components/FilterButtons.tsx
+++ components/FilterButtons.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
 import AddToCartButton from './AddToCartButton'
 
 export default function FilterButtons() {
   return (
     <div className='flex justify-between overflow-scroll text-sm p-2 items-center'>
       <AddToCartButton title = "All"/>
-      <AddToCartButton title = "Mangoes"/>
-      <AddToCartButton title = "Bananas"/>
-      <AddToCartButton title = "Melons"/>
+      <AddToCartButton title = "Vegetables"/>
+      <AddToCartButton title = "Fruits"/>
+      <AddToCartButton title = "Dairy"/>
+      <AddToCartButton title = "Grains"/>
+      <AddToCartButton title = "Poultry"/>
+      <AddToCartButton title = "Meat"/>
     </div>
   )
 }
