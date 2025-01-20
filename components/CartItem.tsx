27117f3d9e91bc74930ecd9dbad293c1c39689da
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-01-20 19:13:42 +0300

    updated price

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -2,17 +2,25 @@
 import { useCartStore } from '@/store/store'
 import Image from 'next/image'
 import React from 'react'
+import { FaArrowLeftLong } from 'react-icons/fa6'
 import { RiDeleteBin5Line } from 'react-icons/ri'
 
 export default function CartItem() {
   const {items,handleRemove,updateQty} = useCartStore((state)=> state)
+
+  const totalExpression = items.map(item => `(${item.price} * ${item.quantity})`).join(' + ');
+  const total = eval(totalExpression);
+
   return (
-    <div className='px-2'>
+    <div>
+      <p className='py-4 bg-white flex justify-between items-center px-3 font-bold'>Sub total items ({items.length} items) <span>Total: $ {total?? 0}</span></p>
+      <button className='m-2 px-6 py-2 bg-transparent border-[1px] border-green-800 flex justify-between items-center gap-2'><FaArrowLeftLong /> Continue Shopping</button>
       {
         items.map((item)=> {
+          const newPrice = item.quantity * item.price
           return(
     <div key={item.id} className='bg-white shadow-lg p-2 mt-2 mb-2'>
-            <div className="flex gap-3 justify-between">
+            <div className="flex gap-3 justify-between px-2">
                 <Image src={item.image} alt={item.title} width={300} height={300} className='w-[200px] h-[100px] object-contain'/>
                 <p className='text-sm flex flex-col'>{item.title}<span className='mt-2 font-bold'>sold by Samuel</span></p>
             </div>
@@ -25,7 +33,7 @@ export default function CartItem() {
                         <button onClick={()=> updateQty("increment",item.id)} className='border-[1px] border-gray-400 py-1 px-3'>+</button>
                     </div>
                 </div>
-                <p className='font-bold flex flex-col'>Price:${item.price} <button onClick={()=> handleRemove(item.id)} className='font-normal border-[1px] border-red-500 py-1 px-6 flex justify-between items-center gap-3 text-red-500'><span className='text-red-500 text-2xl'><RiDeleteBin5Line /></span>Remove Item</button></p>
+                <p className='font-bold flex flex-col'>Price:${newPrice} <button onClick={()=> handleRemove(item.id)} className='font-normal border-[1px] border-red-500 py-1 px-6 flex justify-between items-center gap-3 text-red-500'><span className='text-red-500 text-2xl'><RiDeleteBin5Line /></span>Remove Item</button></p>
             </div>
         </div>
           )
